4100e1f63240f07dcce05ce5ee92c44a08514c8a
Josh McKelvey <jmckelvey0@gmail.com>
2015-10-01 07:48:19 -0400

    add a few tests, about half of them

--- test/models/game_bank_test.rb
+++ test/models/game_bank_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class GameBankTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "the nummies" do
+    a = GameBank.create()
+    b = GameBank.create()
+    assert_equal GameBank.nummies, 4
+  end
+
+  test "debits list" do
+    a = GameBank.create(trans_type: 'D')
+    assert_equal GameBank.debits_list.length, 2
+  end
+
+  test "credits list" do
+    a = GameBank.create(trans_type: 'C')
+    assert_equal GameBank.credits_list.length, 2
+  end
 end
