48ea2524583d4402d11a294d86d02b611d004efe
Josh McKelvey <jmckelvey0@gmail.com>
2015-10-01 07:03:28 -0400

    four things done

--- app/models/game_bank.rb
+++ app/models/game_bank.rb
@@ -14,18 +14,26 @@ class GameBank < ActiveRecord::Base
   end
 
   def self.dtots
-    debits_list.reduce(0) {|s, c| s + c.amount }
+    debits_list.reduce(0) { |s, c| s + c.amount }
   end
 
   def self.ctots
-    credits_list.reduce(0) {|s, c| s + c.amount }
+    credits_list.reduce(0) { |s, c| s + c.amount }
   end
 
   def self.balance
     self.ctots - self.dtots
   end
 
-  def self.dtots_this_month
-      
+  def self.gameover?
+    balance < 0
+  end
+
+  def self.max_expense
+    self.debits_list.sort.first.amount
+  end
+
+  def self.big_spender
+    # Find sum of amount by party
   end
 end
